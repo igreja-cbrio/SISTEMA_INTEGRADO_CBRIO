eeb7dc7694b81c8521fd2b77ce0a75d47490d896
Marcos Paulo Almeida <marcospaulo.almeida@cbrio.org>
2026-08-11 12:01:03 -0300

    fix(santander): usar endpoint oficial de transacoes

--- backend/services/santander/contasService.test.js
+++ backend/services/santander/contasService.test.js
@@ -1,14 +1,10 @@
 const assert = require('node:assert/strict');
-const { statementCompatibilityPath, statementPath } = require('./contasService');
+const { transactionsPath } = require('./contasService');
 
 assert.equal(
-  statementPath({ bankId: '90400888000142', agencia: '3957', conta: '130004222' }),
-  '/bank_account_information/v1/banks/banks/90400888000142/statements/3957.000130004222',
+  transactionsPath({ agencia: '3957', conta: '130004222' }),
+  '/bank_account_information/v1/transactions/3957.000130004222',
 );
-assert.throws(() => statementPath({ bankId: '', agencia: '3957', conta: '130004222' }), /SANTANDER_BANK_ID/);
-assert.equal(
-  statementCompatibilityPath({ bankId: '90400888000142', agencia: '3957', conta: '130004222' }),
-  '/bank_account_information/v1/banks/90400888000142/statements/3957.000130004222',
-);
+assert.throws(() => transactionsPath({ agencia: '', conta: '130004222' }), /SANTANDER_AGENCIA/);
 
 console.log('contasService: ok');
